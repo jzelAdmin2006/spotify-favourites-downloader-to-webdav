4c2c5d923e755212f28c578f72fdd68a0f4bcfa0
jzelAdmin2006 <jo.zeller@protonmail.com>
2023-10-10 21:09:54 +0200

    initialise project

---
+++ src/main/java/org/jzel/spotifyfavouritesdownloadertowebdav/SpotifyFavouritesDownloaderToWebDavApplication.java
@@ -0,0 +1,13 @@
+package org.jzel.spotifyfavouritesdownloadertowebdav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyFavouritesDownloaderToWebDavApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotifyFavouritesDownloaderToWebDavApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/jzel/spotifyfavouritesdownloadertowebdav/SpotifyFavouritesDownloaderToWebDavApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jzel.spotifyfavouritesdownloadertowebdav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyFavouritesDownloaderToWebDavApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
