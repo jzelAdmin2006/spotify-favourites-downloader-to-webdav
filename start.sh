e20f79e74ebc859f7e9b0e80814f8ebe73b85549
Johannes Zeller <jo.zeller@protonmail.com>
2023-12-25 10:41:51 +0100

    temporarily remove files with CRLF line endings that cause issues

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo '{ "clientId": "'"$CLIENT_ID"'", "clientSecret": "'"$CLIENT_SECRET"'" }' > /app/config/spotify/config.json
-echo "$DEEZER_ARL" > /app/config/.arl
-java -jar /app/app.jar
