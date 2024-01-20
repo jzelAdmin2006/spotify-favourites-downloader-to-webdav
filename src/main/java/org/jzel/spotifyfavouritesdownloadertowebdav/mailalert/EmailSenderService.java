458b5511c6e696f2a95c9ea180b3bc0cdd889449
Johannes Zeller <jo.zeller@protonmail.com>
2024-01-20 18:45:48 +0100

    inform user about application startup

--- src/main/java/org/jzel/spotifyfavouritesdownloadertowebdav/mailalert/EmailSenderService.java
+++ src/main/java/org/jzel/spotifyfavouritesdownloadertowebdav/mailalert/EmailSenderService.java
@@ -1,6 +1,8 @@
 package org.jzel.spotifyfavouritesdownloadertowebdav.mailalert;
 
 import lombok.AllArgsConstructor;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -10,6 +12,17 @@ import org.springframework.stereotype.Service;
 public class EmailSenderService {
   private final JavaMailSender mailSender;
   private final MailConfig mailConfig;
+
+  @EventListener(ApplicationReadyEvent.class)
+  public void sendStartupAlert() {
+    send("\uD83D\uDC31 Spotify Favourites Downloader to WebDav gestartet (Erstanmeldung erforderlich)!",
+        """
+            Der Spotify Favourites Downloader to WebDav Server wurde gestartet.
+            
+            Bitte unter folgendem Link anmelden:
+            
+            """ + mailConfig.getReauthUrl());
+  }
 
   public void sendReauthAlert() {
     send("\uD83D\uDE40 Spotify Favourites Downloader to WebDav Neuanmeldung erforderlich!",
